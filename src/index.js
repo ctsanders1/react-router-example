eda438d2dc74f3e00cb88f5e1653c5c6f73d1b72
dankoknad <dankomilutinovic@gmail.com>
2018-06-05 13:02:49 +0200

    moving routes to routesConfig

--- src/index.js
+++ src/index.js
@@ -2,17 +2,9 @@ import React from 'react';
 import {render} from 'react-dom';
 import { Router, Route, Switch } from 'react-router-dom';
 import App from './App';
-import Welcome from './pages/Welcome';
-import Home from './pages/Home';
-import Films from './pages/Films';
-import Characters from './pages/Characters';
-import Planets from './pages/Planets';
-import Species from './pages/Species';
-import Starships from './pages/Starships';
-import Vehicles from './pages/Vehicles';
-import NotFound from './pages/NotFound';
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
+import { routes } from './routesConfig';
 
 import createBrowserHistory from 'history/createBrowserHistory'
 const history = createBrowserHistory({ basename: '/react-router-example' });
@@ -21,15 +13,14 @@ render((
   <Router history={history}>
     <App>
       <Switch>
-      <Route path="/" component={Welcome} exact/>
-      <Route path="/home" component={Home}/>
-      <Route path="/films" component={Films}/>
-      <Route path="/characters" component={Characters}/>
-      <Route path="/species" component={Species}/>
-      <Route path="/planets" component={Planets}/>
-      <Route path="/starships" component={Starships}/>
-      <Route path="/vehicles" component={Vehicles}/>
-      <Route path="*" component={NotFound}/>
+        {routes.map(route => 
+          <Route
+            key={route.path}
+            path={route.path}
+            component={route.component}
+            exact={route.exact}
+          />
+        )}
       </Switch>
     </App>
   </Router>
---
+++ src/routesConfig.js
@@ -0,0 +1,43 @@
+import Welcome from './pages/Welcome';
+import Home from './pages/Home';
+import Films from './pages/Films';
+import Characters from './pages/Characters';
+import Planets from './pages/Planets';
+import Species from './pages/Species';
+import Starships from './pages/Starships';
+import Vehicles from './pages/Vehicles';
+import NotFound from './pages/NotFound';
+
+export const routes = [
+  {
+    path: "/",
+    component: Welcome,
+    exact: true
+  },
+  {
+    path: "/home",
+    component: Home
+  },{
+    path: "/films",
+    
+    component: Films
+  },{
+    path: "/characters",
+    component: Characters
+  },{
+    path: "/species",
+    component: Species
+  },{
+    path: "/planets",
+    component: Planets
+  },{
+    path: "/starships",
+    component: Starships
+  },{
+    path: "/vehicles",
+    component: Vehicles
+  },{
+    path: "*",
+    component: NotFound
+  }
+]
